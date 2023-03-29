6ef74bfd9f2828ad907c2d4b574fe44b1698f703
Leen4k <110277798+Leen4k@users.noreply.github.com>
2023-03-29 18:58:55 +0700

    second commit

--- tempCodeRunnerFile.java
+++
@@ -1,6 +0,0 @@
-
-      // for (r.start=r.start; r.start <= r.end; r.start+=r.step) 
-      // {    
-      //    if (r.start % 2 != 0)  
-      //       System.out.print(r.start + " ");   
-      // }    
